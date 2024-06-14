0d7e6b2d09752d693770d9fdb1398aebd93994c4
mooncake <adom.donatas@gmail.com>
2024-06-14 19:47:53 +0300

    Improve home view list display style

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import NavigationBar from './components/NavigationBar.vue';
 </script>
 
 <template>
-  <div class="max-w-screen-xl mx-auto px-4 sm:px-8">
+  <div class="max-w-screen-xl mx-auto px-4 sm:px-8 mb-32">
     <NavigationBar class="mb-2" />
 
     <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,11 +9,22 @@ const { topPlanets, commonVehicles, loading } = useTopPlanets();
   <div>
     <div v-if="loading">Loading Top Populated Planets Data...</div>
     <div v-else>
-      <h1>Top Populated Planets</h1>
-      <ul>
-        <li v-for="planet in topPlanets" :key="planet.name">{{ planet.name }} - Population: {{ planet.population }}</li>
-      </ul>
-      <h2 class="mt-5">Common Vehicles of Top 3 Planets:</h2>
+      <h2 class="text-xl font-bold mb-2">Top Populated Planets</h2>
+
+      <div class="flow-root mt-6 mb-10">
+        <dl class="-my-3 divide-y divide-gray-100 text-sm">
+          <div
+            v-for="planet in topPlanets"
+            :key="planet.name"
+            class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4"
+          >
+            <dt class="font-medium text-gray-900">{{ planet.name }}</dt>
+            <dd class="text-gray-700 sm:col-span-2">Population: {{ planet.population }}</dd>
+          </div>
+        </dl>
+      </div>
+
+      <h2 class="mt-5 text-xl font-bold">Common Vehicles of Top 3 Planets:</h2>
       <VehicleChart :commonVehicles="commonVehicles" />
     </div>
   </div>
