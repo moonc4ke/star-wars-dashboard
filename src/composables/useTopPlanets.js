844e49e93f2321cf80ed3cd288771f99834126b6
mooncake <adom.donatas@gmail.com>
2024-06-14 19:37:35 +0300

    Improve updateCommonVehicles and loadTopPlanets methods

--- src/composables/useTopPlanets.js
+++ src/composables/useTopPlanets.js
@@ -8,34 +8,46 @@ export default function useTopPlanets() {
 
   async function loadTopPlanets() {
     loading.value = true;
-    const allPlanets = await fetchPlanets();
-    const sortedPlanets = allPlanets
-      .filter((p) => p.population !== 'unknown')
-      .sort((a, b) => Number(b.population) - Number(a.population));
-    topPlanets.value = sortedPlanets.slice(0, 3);
+    try {
+      const allPlanets = await fetchPlanets();
+      const sortedPlanets = allPlanets
+        .filter((p) => p.population !== 'unknown')
+        .sort((a, b) => Number(b.population) - Number(a.population));
+      topPlanets.value = sortedPlanets.slice(0, 3);
 
+      await updateCommonVehicles();
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function updateCommonVehicles() {
     const residentUrls = topPlanets.value.flatMap((p) => p.residents).filter((url) => url);
     if (residentUrls.length > 0) {
       const residents = await fetchResidents(residentUrls);
-      const vehicleUrls = residents.map((r) => r.vehicles).filter((v) => v.length > 0);
+      const vehicleUrls = residents.flatMap((r) => r.vehicles).filter((v) => v);
 
       if (vehicleUrls.length > 0) {
-        const allVehicleUrls = vehicleUrls.flat();
-        const vehicleFrequency = allVehicleUrls.reduce((acc, url) => {
+        const vehicleCounts = vehicleUrls.reduce((acc, url) => {
           acc[url] = (acc[url] || 0) + 1;
           return acc;
         }, {});
 
-        const commonVehicleUrls = Object.keys(vehicleFrequency).filter(
-          (url) => vehicleFrequency[url] === topPlanets.value.length
-        );
+        const commonVehicleUrls = Object.entries(vehicleCounts)
+          .filter(([, count]) => count === topPlanets.value.length)
+          .map(([url]) => url);
+
         if (commonVehicleUrls.length > 0) {
           const vehicles = await fetchVehicles();
-          commonVehicles.value = vehicles.filter((v) => commonVehicleUrls.includes(v.url));
+          commonVehicles.value = vehicles
+            .filter((v) => commonVehicleUrls.includes(v.url))
+            .map((v) => ({
+              name: v.name,
+              count: vehicleCounts[v.url],
+            }));
         }
       }
     }
-    loading.value = false;
   }
 
   onMounted(loadTopPlanets);
